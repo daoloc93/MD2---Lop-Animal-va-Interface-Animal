8b3df2a343e879537524ee2d7306bcc9a11c09f2
Loc <daoloc93@gmail.com>
2022-04-01 16:41:09 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(567, 655, 768);
+        System.out.println(triangle.toString());
+    }
+}
